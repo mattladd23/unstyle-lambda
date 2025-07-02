6e5986523a7374da5388346b772f0885e3b8021b
Matt <matt-jbl@hotmail.co.uk>
2025-07-02 18:15:16 +0100

    Refactored to then catch

--- index.mjs
+++ index.mjs
@@ -1,31 +1,25 @@
 import AWS from 'aws-sdk';
 const s3 = new AWS.S3();
 const BUCKET_NAME = 'unstyle-json';
-const OBJECT_KEY = 'db.json';
-export async function handler(event) {
-    try {
-        const data = await s3.getObject({
-            Bucket: BUCKET_NAME,
-            Key: OBJECT_KEY
-        }).promise();
-        const json = JSON.parse(data.Body?.toString('utf-8') || '{}');
-        const initialValues = json.initialValues ?? [];
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ initialValues })
-        };
-    }
-    catch (error) {
-        console.error('S3 error:', error);
-        return {
-            statusCode: 500,
-            headers: { 'Access-Control-Allow-Origin': '*' },
-            body: JSON.stringify({ error: 'Failed to retrieve data' })
-        };
-    }
-}
+const OBJECT_KEY = 'initialValues.json';
+export const handler = async (event) => s3.getObject({ Bucket: BUCKET_NAME, Key: OBJECT_KEY }).promise()
+    .then(res => {
+    const json = JSON.parse(res.Body?.toString('utf-8') || '{}');
+    const initialValues = json.initialValues ?? [];
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ initialValues })
+    };
+})
+    .catch(error => {
+    return {
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({ error: `Failed to retrieve data: ${error}` })
+    };
+});
 //# sourceMappingURL=index.mjs.map
